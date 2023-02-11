ff221cb3cd4f94b5fccc3a5841115b3ea74afe42
Rod Hyde <rod.hyde@gmail.com>
2023-02-11 14:55:03 +0000

    Extract dispatcher.
    - Extract the nascent dispatcher.
    - Correct a typo (for some reason I can't type "disassembler").

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use arviss_experiment::{decode, Disassembler};
 
 pub fn main() {
-    let mut dissassembler = Disassembler {};
+    let mut disassembler = Disassembler {};
 
     for ins in [
         // _start:
@@ -28,58 +28,7 @@ pub fn main() {
         0x00_c0_80_e7, // jalr  12(ra)
         0x00_10_00_73, // ebreak
     ] {
-        let result = decode(&mut dissassembler, ins);
+        let result = decode(&mut disassembler, ins);
         println!("{:08x} {}", ins, result);
     }
 }
-
-// TODO: These exist for *testing*. Move them into a testing module.
-#[derive(Debug)]
-enum ArvissOpcode {
-    OpLUI = 0b0110111,
-    OpAUIPC = 0b0010111,
-    OpJAL = 0b1101111,
-    OpJALR = 0b1100111,
-    OpBRANCH = 0b1100011,
-    OpLOAD = 0b0000011,
-    OpSTORE = 0b0100011,
-    OpOPIMM = 0b0010011,
-    OpOP = 0b0110011,
-    OpMISCMEM = 0b0001111,
-    OpSYSTEM = 0b1110011,
-    OpLOADFP = 0b0000111,  // RV32F
-    OpSTOREFP = 0b0100111, // RV32F
-    OpOPFP = 0b1010011,    // RV32F
-    OpMADD = 0b1000011,    // RV32F
-    OpMSUB = 0b1000111,    // RV32F
-    OpNMSUB = 0b1001011,   // RV32F
-    OpNMADD = 0b1001111,   // RV32F
-}
-
-trait Dispatcher {
-    fn exec_add(&mut self, rd: u8, rs1: u8, imm: i32);
-    fn exec_ecall(&mut self);
-}
-
-// impl Dispatcher for Disassembler {
-//     fn exec_add(&mut self, rd: u8, rs1: u8, imm: i32) {
-//         println!("add {}, {}, {}", rd, rs1, imm);
-//     }
-
-//     fn exec_ecall(&mut self) {
-//         println!("ecall");
-//     }
-// }
-
-// fn dispatch(dispatcher: &mut impl Dispatcher, ins: DecodedInstruction) {
-//     match ins {
-//         DecodedInstruction::RdRs1Imm {
-//             opcode: ExecAddi,
-//             rd,
-//             rs1,
-//             imm,
-//         } => dispatcher.exec_add(rd, rs1, imm),
-//         DecodedInstruction::NoArgs { opcode: ExecEcall } => dispatcher.exec_ecall(),
-//         _ => {}
-//     }
-// }
